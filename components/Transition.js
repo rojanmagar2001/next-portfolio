734423f3c604c6b8a476a31d233705dadcfeba15
Rojan Rana Magar <rojan.magar@aitc.ai>
2023-07-04 20:48:13 +0545

    changes

--- components/Transition.js
+++ components/Transition.js
@@ -4,15 +4,15 @@ import { motion } from "framer-motion";
 const transitionVariants = {
   initial: {
     x: "100%",
-    widht: "100%",
+    width: "100%",
   },
   animate: {
     x: "0%",
-    widht: "0%",
+    width: "0%",
   },
   exit: {
     x: ["0%", "100%"],
-    widht: ["0%", "100%"],
+    width: ["0%", "100%"],
   },
 };
 
