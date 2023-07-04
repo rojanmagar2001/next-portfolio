e5b4002c317e8f4d589c376183e9bc87e799c3cf
Rojan Rana Magar <rojan.magar@aitc.ai>
2023-07-04 20:18:58 +0545

    contact page

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -7,16 +7,16 @@ const ProjectsBtn = () => {
     <div className="mx-auto xl:mx-0">
       <Link
         href={"/work"}
-        className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
+        className="relative w-[140px] h-[140px]  xl:w-[185px] xl:h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
       >
         <Image
           src={"/rounded-text.png"}
           width={141}
           height={148}
           alt=""
-          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+          className="animate-spin-slow w-full h-full max-w-[115px] max-h-[115px]  xl:max-w-[141px] xl:max-h-[148px]"
         />
-        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
+        <HiArrowRight className="absolute text-3xl xl:text-4xl group-hover:translate-x-2 transition-all duration-300" />
       </Link>
     </div>
   );
